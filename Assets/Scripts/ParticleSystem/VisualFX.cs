5e45679c2ae4a529bd71cdbec1610ee5b40e3b79
Pierre Malatesta <s210410@students.aie.edu.au>
2022-04-11 13:44:42 +0930

    till working on particle system

--- Assets/Scripts/ParticleSystem/VisualFX.cs
+++ Assets/Scripts/ParticleSystem/VisualFX.cs
@@ -1,51 +1,26 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//namespace VisualFXSystem
-//{
-//    [CreateAssetMenu(fileName = "VisualFX", menuName = "VisualFX/VisualFX", order = 1)]
-//    public class VisualFX : ScriptableObject
-//    {
-//        public GameObject prefab;
-//        public float duration;
-//    }
+namespace VisualFXSystem
+{
+    [CreateAssetMenu(fileName = "VisualFX", menuName = "VisualFX/VisualFX", order = 1)]
+    public class VisualFX : ScriptableObject
+    {
+        public GameObject prefab;
+        public float duration;
+        private bool autoStop;
 
-//    public class VisualFXInstance : MonoBehaviour
-//    {
-//        float countdown;
-//        public bool countingDown;
+        public VisualFXInstance Begin(Transform t)
+        {
+            GameObject obj = Instantiate(prefab, t);
 
-//        public void Init(VisualFX fx, bool autoStop)
-//        {
-//            countingDown = autoStop;
-//            countdown = fx.duration;
-//        }
-
-//        public void Update()
-//        {
-//            if (!countingDown)
-//                return false;
-
-//            countdown -= Time.deltaTime;
-
-//            if (countdown < 0)
-//            {
-//                GameObject.Destroy(gameObject);
-//            }
-//        }
-
-//        public bool isFinished() { return countdown <= 0; }
-//    }
-//    public VisualFXInstance Begin(Transform t)
-//    {
-//        GameObject obj = Instantiate(prefab, t);
-
-//        VisualFXInstance instance = obj.GetComponent<VisualFXInstance>();
-//        if (instance == null)
-//            instance = obj.AddComponent<VisualFXInstance>();
-//        instance.Init(this, autoStop);
-//        return instance;
-//    }
-//}
+            VisualFXInstance instance = obj.GetComponent<VisualFXInstance>();
+            if (instance == null)
+                instance = obj.AddComponent<VisualFXInstance>();
+            instance.Init(this, autoStop);
+            return instance;
+        }
+    }
+}
 
